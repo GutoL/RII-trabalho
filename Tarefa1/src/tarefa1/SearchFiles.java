0b8c9cb12de341e9dc6cb9d4d52c0dbcf52c9d12
Guto Leoni <guto.leoni@gprt.ufpe.br>
2017-09-04 16:05:05 -0300

    adicionando interface gráfica (com erros)

--- Tarefa1/src/tarefa1/SearchFiles.java
+++ Tarefa1/src/tarefa1/SearchFiles.java
@@ -42,10 +42,10 @@ import org.apache.lucene.store.FSDirectory;
 /** Simple command-line based search demo. */
 public class SearchFiles {
 
-  private SearchFiles() {}
+  public SearchFiles() {}
 
   /** Simple command-line based search demo. */
-  public static void main(String[] args) throws Exception {
+  public void search(String line) throws Exception {
     
     String index = "index";
     String field = "contents";
@@ -55,34 +55,6 @@ public class SearchFiles {
     String queryString = null;
     int hitsPerPage = 10;
     
-    for(int i = 0;i < args.length;i++) {
-      if ("-index".equals(args[i])) {
-        index = args[i+1];
-        i++;
-      } else if ("-field".equals(args[i])) {
-        field = args[i+1];
-        i++;
-      } else if ("-queries".equals(args[i])) {
-        queries = args[i+1];
-        i++;
-      } else if ("-query".equals(args[i])) {
-        queryString = args[i+1];
-        i++;
-      } else if ("-repeat".equals(args[i])) {
-        repeat = Integer.parseInt(args[i+1]);
-        i++;
-      } else if ("-raw".equals(args[i])) {
-        raw = true;
-      } else if ("-paging".equals(args[i])) {
-        hitsPerPage = Integer.parseInt(args[i+1]);
-        if (hitsPerPage <= 0) {
-          System.err.println("There must be at least 1 hit per page.");
-          System.exit(1);
-        }
-        i++;
-      }
-    }
-    
     IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(index)));
     IndexSearcher searcher = new IndexSearcher(reader);
     
@@ -91,46 +63,50 @@ public class SearchFiles {
     //Analyzer analyzer = new  EnglishAnalyzer(CharArraySet.EMPTY_SET); // without stopwords filter/ with stemming
     Analyzer analyzer = new EnglishAnalyzer();                          // with stop words/with stemming
     
-    BufferedReader in = null;
-    if (queries != null) {
-      in = Files.newBufferedReader(Paths.get(queries), StandardCharsets.UTF_8);
-    } else {
-      in = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8));
-    }
+    
     QueryParser parser = new QueryParser(field, analyzer);
+    
     while (true) {
-      if (queries == null && queryString == null) {                        // prompt the user
-        System.out.println("Enter query: ");
-      }
-
-      String line = queryString != null ? queryString : in.readLine();
-
-      if (line == null || line.length() == -1) {
-        break;
-      }
-
-      line = line.trim();
-      if (line.length() == 0) {
-        break;
-      }
       
-      Query query = parser.parse(line);
-      System.out.println("Searching for: " + query.toString(field));
-            
-      if (repeat > 0) {                           // repeat & time as benchmark
-        Date start = new Date();
-        for (int i = 0; i < repeat; i++) {
-          searcher.search(query, 100);
+        if (queries == null && queryString == null) {                        // prompt the user
+            System.out.println("Enter query: ");
         }
-        Date end = new Date();
-        System.out.println("Time: "+(end.getTime()-start.getTime())+"ms");
-      }
 
-      doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
+        if (line == null || line.length() == -1) {
+            break;
+        }
 
-      if (queryString != null) {
-        break;
-      }
+        line = line.trim();
+        
+        if (line.length() == 0) {
+            break;
+        }
+      
+        Query query = parser.parse(line);
+        System.out.println("Searching for: " + query.toString(field));
+            
+        if (repeat > 0) {                           // repeat & time as benchmark
+            
+            Date start = new Date();
+            
+            for (int i = 0; i < repeat; i++) {
+                searcher.search(query, 100);
+            }
+        
+            Date end = new Date();
+            System.out.println("Time: "+(end.getTime()-start.getTime())+"ms");
+        }
+
+        //doPagingSearch(in, searcher, query, hitsPerPage, raw, queries == null && queryString == null);
+        TopDocs results = searcher.search(query, 5 * hitsPerPage);
+        ScoreDoc[] hits = results.scoreDocs;
+    
+    int numTotalHits = results.totalHits;
+    System.out.println(numTotalHits + " total matching documents");
+        
+        if (queryString != null) {
+            break;
+        }
     }
     reader.close();
   }
